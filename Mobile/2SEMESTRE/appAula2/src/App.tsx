6bc625f4d09ad12eabd30511ebf561364868ffb7
DutraGames <gabrieldutraamarante@gmail.com>
2025-02-25 17:26:53 -0300

    feat: add bg-image in app

--- Mobile/2SEMESTRE/appAula2/src/App.tsx
+++ Mobile/2SEMESTRE/appAula2/src/App.tsx
@@ -1,22 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 import Comp01 from "./components/comp01";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ImageBackground
+      source={require("./assets/bg.jpg")}
+      style={styles.container}
+    >
       <Text>App aula 3</Text>
       <Comp01 />
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
