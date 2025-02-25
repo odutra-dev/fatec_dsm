00e8cf987092e6e0c50fe6376dc4acd0f95472a7
DutraGames <gabrieldutraamarante@gmail.com>
2025-02-25 17:42:53 -0300

    feat: add onPress action componet 2 test

--- Mobile/2SEMESTRE/appAula2/src/components/comp02/index.tsx
+++ Mobile/2SEMESTRE/appAula2/src/components/comp02/index.tsx
@@ -1,9 +1,13 @@
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, Alert } from "react-native";
 export default function index({ title }: { title: string }) {
+  const handlePress = () => {
+    Alert.alert("Apertou o botao: " + title);
+  };
+
   return (
     <View>
       <Text>Componente 02</Text>
-      <Button title={title} color={"#000"} />
+      <Button title={title} color={"#000"} onPress={handlePress} />
     </View>
   );
 }
