054dc089c325478ed754e278ab8bca8989759eb7
DutraGames <gabrieldutraamarante@gmail.com>
2025-04-29 23:56:15 -0300

    refactor: add min in password

--- Cibersecurity/blog-simple/src/schemas/user.schema.ts
+++ Cibersecurity/blog-simple/src/schemas/user.schema.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 export const userCreateSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  password: z.string(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
 export const userSchema = z.object({
