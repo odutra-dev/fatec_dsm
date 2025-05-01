e4198568e3bb8734dd3e213275379b0d46055cbb
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-01 19:04:56 -0300

    feat: add calc for triangle page

---
+++ Mobile/2SEMESTRE/geometrix/src/app/triangulo/index.tsx
@@ -0,0 +1,134 @@
+import { useTheme } from "@/context/theme";
+import { useEffect, useState } from "react";
+import { TouchableOpacity } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  interpolateColor,
+  withTiming,
+} from "react-native-reanimated";
+
+import { Colors } from "@/theme/colors";
+import { Field } from "@/components/field";
+const ButtonAnimated = Animated.createAnimatedComponent(TouchableOpacity);
+
+export default function Triangulo() {
+  const { isDark, toggleTheme } = useTheme();
+
+  const [ladoA, setLadoA] = useState();
+  const [ladoB, setLadoB] = useState<number>();
+  const [ladoC, setLadoC] = useState<number>();
+  const [value, setValue] = useState<number>();
+  const [perimetro, setPerimetro] = useState<number>();
+  const [area, setArea] = useState<number>();
+
+  const themeAnimated = useSharedValue(isDark ? 1 : 0);
+
+  useEffect(() => {
+    themeAnimated.value = withTiming(isDark ? 1 : 0, { duration: 800 });
+  }, [isDark]);
+
+  const viewAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        themeAnimated.value,
+        [0, 1],
+        [Colors.light.background, Colors.dark.background]
+      ),
+      flex: 1,
+      paddingHorizontal: 24,
+      marginTop: 24,
+    };
+  });
+
+  const textPrimaryAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        themeAnimated.value,
+        [0, 1],
+        [Colors.light.primary, Colors.dark.primary]
+      ),
+      fontSize: 24,
+      fontWeight: "bold",
+      textAlign: "center",
+    };
+  });
+
+  const textSecondaryAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        themeAnimated.value,
+        [0, 1],
+        [Colors.light.secundary, Colors.dark.secudary]
+      ),
+      fontSize: 16,
+    };
+  });
+
+  const textTextAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        themeAnimated.value,
+        [0, 1],
+        [Colors.light.text, Colors.dark.text]
+      ),
+      fontSize: 16,
+      fontWeight: "bold",
+    };
+  });
+
+  const buttonAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        themeAnimated.value,
+        [0, 1],
+        [Colors.light.primary, Colors.dark.primary]
+      ),
+
+      padding: 12,
+      borderRadius: 8,
+      alignItems: "center",
+      marginVertical: 12,
+    };
+  });
+
+  return (
+    <Animated.View style={[viewAnimatedStyle]}>
+      <Animated.Text style={[textPrimaryAnimatedStyle]}>
+        Retangulo
+      </Animated.Text>
+      <Animated.Image
+        style={{
+          width: 200,
+          height: 200,
+          alignSelf: "center",
+          resizeMode: "contain",
+          marginVertical: 12,
+        }}
+        source={{
+          uri: "https://uploads-cdn.omnicalculator.com/images//geometry/perimeter/triangle1-r2.svg?width=425&enlarge=0&format=webp",
+        }}
+      ></Animated.Image>
+      <Field nome="Lado A" value={ladoA} setValue={setLadoA} />
+      <Field nome="Lado B" value={ladoB} setValue={setLadoB} />
+      <Field nome="Lado C" value={ladoC} setValue={setLadoC} />
+      <ButtonAnimated
+        style={[buttonAnimatedStyle]}
+        onPress={() => {
+          setPerimetro(parseInt(ladoA) + parseInt(ladoB) + parseInt(ladoC));
+          setArea((ladoA * ladoB) / 2);
+        }}
+      >
+        <Animated.Text style={[textTextAnimatedStyle]}>Calcular</Animated.Text>
+      </ButtonAnimated>
+
+      <Animated.Text style={[textSecondaryAnimatedStyle]}>
+        Perímetro: {perimetro}
+      </Animated.Text>
+      <Animated.Text style={[textSecondaryAnimatedStyle]}>
+        {" "}
+        Área: {area}
+      </Animated.Text>
+    </Animated.View>
+  );
+}
