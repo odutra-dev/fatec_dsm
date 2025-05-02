c8f5bbc6237c8ce80ee2783511557a9bd1c07fe7
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-01 23:13:58 -0300

    chore: commit initial front-end

--- Cibersecurity/blog-simple/src/types/post.type.ts
+++
@@ -1,5 +0,0 @@
-import { postCreateSchema, postSchema } from "../schemas/post.schema";
-import { z } from "zod";
-
-export type PostCreate = z.infer<typeof postCreateSchema>;
-export type PostType = z.infer<typeof postSchema>;
