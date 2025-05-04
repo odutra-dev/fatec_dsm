52b093efba12a3056f11ef9f89f58873b4a272d6
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-04 20:56:32 -0300

    feat: add verify infos for me route

--- Cibersecurity/blog-simple/back-end/src/routes/user.route.ts
+++ Cibersecurity/blog-simple/back-end/src/routes/user.route.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 import { userCreateSchema, userLoginSchema } from "../schemas/user.schema";
 import { FastifyTypedInstance } from "../types/FastifyTipeInstance";
 import { UserUsecase } from "../usecase/user.usecase";
@@ -5,6 +6,21 @@ import { hashPassword } from "../util/hash";
 
 export const userRoute = async (app: FastifyTypedInstance) => {
   const userUseCase = new UserUsecase();
+
+  app.addHook(
+    "onRequest",
+
+    async (request, reply) => {
+      try {
+        if (request.method === "GET") {
+          await request.jwtVerify();
+        }
+      } catch (error) {
+        reply.code(403).send(error);
+      }
+    }
+  );
+
   app.post(
     "/register",
     {
@@ -54,6 +70,34 @@ export const userRoute = async (app: FastifyTypedInstance) => {
       return reply.code(200).send({
         token,
       });
+    }
+  );
+
+  app.get(
+    "/user/:id",
+    {
+      schema: {
+        description: "Get a user by id",
+        tags: ["User"],
+        headers: z.object({
+          authorization: z.string().regex(/^Bearer\s.+$/, {
+            message: "Authorization header must start with 'Bearer '",
+          }),
+        }),
+        params: z.object({
+          id: z.string().transform(Number),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+      const user = await userUseCase.getById(id);
+
+      if (!user) {
+        return reply.code(404).send({ message: "User not found" });
+      }
+
+      return reply.code(200).send(user);
     }
   );
 };
