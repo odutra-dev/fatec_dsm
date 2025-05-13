832a6707988cb8a4db18f3525d6268e691c93239
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-13 11:23:23 -0300

    refactor: add crypto in contact posr and descrypto get

--- Cibersecurity/storage-contacts/src/contacts/contacts.service.ts
+++ Cibersecurity/storage-contacts/src/contacts/contacts.service.ts
@@ -6,25 +6,37 @@ import {
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 import { prisma } from 'src/lib/prismaClient';
+import { CryptoAES } from '../util/crypto-aes';
 
 @Injectable()
 export class ContactsService {
+  private crypto: CryptoAES = new CryptoAES();
   async create(createContactDto: CreateContactDto) {
+    const encryptedPhone = this.crypto.encrypt(createContactDto.phone);
+    createContactDto.phone = encryptedPhone;
+
+    const encryptedName = this.crypto.encrypt(createContactDto.name);
+    createContactDto.name = encryptedName;
+
     return await prisma.contact.create({ data: createContactDto });
   }
 
   async findOne(id: string) {
-    const contacts = await prisma.contact.findUnique({ where: { id } });
+    const contact = await prisma.contact.findUnique({ where: { id } });
 
     if (!id) {
       throw new BadRequestException('Id is required');
     }
 
-    if (!contacts) {
+    if (!contact) {
       throw new NotFoundException('Contact not found');
     }
 
-    return contacts;
+    // Descriptografar os campos
+    contact.name = this.crypto.decrypt(contact.name);
+    contact.phone = this.crypto.decrypt(contact.phone);
+
+    return contact;
   }
 
   async update(id: string, updateContactDto: UpdateContactDto) {
