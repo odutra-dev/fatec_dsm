453b2acc4898be7a629d5ac66278e978d5b1970a
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-18 23:19:11 -0300

    test: initial test contacts

--- Cibersecurity/storage-contacts/src/contacts/contacts.controller.spec.ts
+++ Cibersecurity/storage-contacts/src/contacts/contacts.controller.spec.ts
@@ -1,17 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
+import { PrismaService } from '../lib/prismaService';
+import { CryptoAES } from '../util/crypto-aes';
+import { UserService } from '../user/user.service';
 
 describe('ContactsController', () => {
   let controller: ContactsController;
+  let prisma: PrismaService;
+  let cryptoAES: CryptoAES;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ContactsController],
-      providers: [ContactsService],
+      providers: [ContactsService, PrismaService, CryptoAES, UserService],
     }).compile();
 
     controller = module.get<ContactsController>(ContactsController);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
--- Cibersecurity/storage-contacts/src/contacts/contacts.service.spec.ts
+++ Cibersecurity/storage-contacts/src/contacts/contacts.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContactsService } from './contacts.service';
+import { ContactsController } from './contacts.controller';
+import { PrismaService } from '../lib/prismaService';
+import { CryptoAES } from '../util/crypto-aes';
+import { UserService } from '../user/user.service';
 
 describe('ContactsService', () => {
-  let service: ContactsService;
+  let service: ContactsController;
+  let prisma: PrismaService;
+  let cryptoAES: CryptoAES;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContactsService],
+      controllers: [ContactsController],
+      providers: [ContactsService, PrismaService, CryptoAES, UserService],
     }).compile();
 
-    service = module.get<ContactsService>(ContactsService);
+    service = module.get<ContactsController>(ContactsController);
+    prisma = module.get<PrismaService>(PrismaService);
+    cryptoAES = module.get<CryptoAES>(CryptoAES);
   });
 
   it('should be defined', () => {
