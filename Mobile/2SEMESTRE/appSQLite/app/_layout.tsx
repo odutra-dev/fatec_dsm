d6eadd9caba869bc9ce953cfc3026b944d3ca39f
DutraGames <gabrieldutraamarante@gmail.com>
2025-05-29 23:46:58 -0300

    refactor: redefine layout

--- Mobile/2SEMESTRE/appSQLite/app/_layout.tsx
+++ Mobile/2SEMESTRE/appSQLite/app/_layout.tsx
@@ -15,7 +15,7 @@ export default function Layout() {
   return (
     <SQLiteProvider databaseName="sistema.db">
       <QueryClientProvider client={queryClient}>
-        <Stack initialRouteName="login">
+        <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="cadastro" options={{ headerShown: false }} />
           <Stack.Screen name="login" options={{ headerShown: false }} />
