f9630fba7ed694714d4bf8f6b32bdf74be123c80
odutra-dev <gabrieldutraamarante@gmail.com>
2025-08-29 16:17:23 -0300

    fix: code run

--- Cloud/2SEMESTRE/api_Docker/flask-api/app.py
+++ Cloud/2SEMESTRE/api_Docker/flask-api/app.py
@@ -1,5 +1,4 @@
 import fastapi
-import uvicorn
 
 app = fastapi.FastAPI()
 
@@ -8,6 +7,3 @@ nome = "Cloud Computing"
 @app.get("/")
 def read_nome():
     return {"Class": nome}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
