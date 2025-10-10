98ceb08a844bc8401fc6726153e7f841a9d76671
Gabriel Dutra <gabriel.carvalho57@fatec.sp.gov.br>
2025-10-10 17:05:31 -0300

    Development (#62)
    * feat: add dockerfile frontend
    
    * feat: activity 6
    
    * feat: add mysql

--- Cibersecurity/blog-simple/back-end/src/lib/prisma.ts
+++ Cibersecurity/blog-simple/back-end/src/lib/prisma.ts
@@ -1,3 +1,12 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  connectionLimit: 5,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
